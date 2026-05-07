9e624cfd33164fdd749bdb8c6cc6eaf9ac3f4e0c
RitikTecholution <ritik.dubey@techolution.com>
2026-05-07 16:47:38 +0530

    feat: Added get projects based on the userID

--- app/api/v1/project.py
+++ app/api/v1/project.py
@@ -11,13 +11,18 @@ router = APIRouter(prefix="/projects", tags=["projects"])
 
 
 # Version 1 controllers
-@router.post("/", response_model=ProjectBase, status_code=status.HTTP_201_CREATED)
-def create_project(project: ProjectBase, user_id: int, db: Session = Depends(get_db)):
-    db_project = Project(title=project.title, description=project.description, user_id=user_id, project_owner_name=project.project_owner_name)
-    db.add(db_project)
-    db.commit()
-    db.refresh(db_project)
-    return db_project
+
+# Get all projects for a user
+@router.get("/user", response_model=list[ProjectResponse])
+def get_projects(user_id: int, db: Session = Depends(get_db)):
+    try:
+        projects = db.query(Project).join(ProjectUser, Project.id == ProjectUser.project_id).filter(ProjectUser.user_id == user_id).all()
+        return projects
+    except Exception as e:
+        logger.error(f"Failed to get projects for user_id {user_id}: {e}")
+        raise HTTPException(status_code=500, detail="Failed to retrieve projects")
+
+
 
 @router.post("/add", response_model=ProjectResponse, status_code=status.HTTP_201_CREATED)
 def add_project(project: ProjectCreate, db: Session = Depends(get_db)):
